e83eacc85f580c0d722e361016caae44113f2972
Laveesha kumra <laveesha171435.cse@chitkara.edu.in>
2019-12-04 11:18:07 +0530

    initial commit

---
+++ react+validation/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Form from './Form.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Form Validation  </h1>
+        <Form />
+        
+      </div>
+    );
+  }
+}
+
+export default App;
